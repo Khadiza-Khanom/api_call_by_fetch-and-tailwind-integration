1e350ef8dbb79223fe568cf26207a8899d9a7bd1
Khadiza-Khanom <khadizakhanommeem@gmail.com>
2024-02-18 17:46:18 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+ let res =ref([])
+const api = 'https://jsonplaceholder.typicode.com/users'
+fetch(api)
+.then(response=>{
+  if(!response.ok){
+    throw new Error('Network problem');
+  }
+  return response.json()
+})
+.then(data=>{
+ res.value = data
+  console.log(data)
+  return res.value
+})
+.catch(error=>{
+  console.error(error)
+})
+
+
+</script>
+
+<template>
+<p class="text-red-500" v-for="ress,index in res" :key="index">{{ ress.id }} --- {{ress.name }}</p>
+
+
+</template>
+
+<style scoped>
+</style>
